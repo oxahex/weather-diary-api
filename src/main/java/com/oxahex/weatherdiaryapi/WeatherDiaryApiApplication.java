7d06812469dcb89a2462007b0da4141448b7d8c9
oxahex <oxahex@gmail.com>
2023-09-24 10:16:06 +0900

    first commit

---
+++ src/main/java/com/oxahex/weatherdiaryapi/WeatherDiaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.oxahex.weatherdiaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDiaryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherDiaryApiApplication.class, args);
+    }
+
+}
